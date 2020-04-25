894b12b0dd768fc8b84a96a36cefcde182af0764
CppPhil <CppPhil@users.noreply.github.com>
2020-04-26 00:04:53 +0200

    Basic attempt

---
+++ src/main.cpp
@@ -0,0 +1,168 @@
+#include <cstdio>
+
+#include <future>
+
+#include "caf/all.hpp"
+#include "caf/io/all.hpp"
+#include "caf/tracing_data_factory.hpp"
+
+struct trc_data : caf::tracing_data
+{
+  std::string s;
+
+  trc_data(std::string str)
+    : s(std::move(str))
+  {
+  }
+
+  caf::error serialize(caf::serializer& sink) const override
+  {
+    return sink(s);
+  }
+
+  caf::error_code<caf::sec>
+  serialize(caf::binary_serializer& sink) const override
+  {
+    return sink(s);
+  }
+};
+
+struct trc_data_fac : caf::tracing_data_factory
+{
+  caf::error
+  deserialize(caf::deserializer& source,
+              std::unique_ptr<caf::tracing_data>& dst) const override
+  {
+    return deserialize_impl(source, dst);
+  }
+
+  caf::error_code<caf::sec>
+  deserialize(caf::binary_deserializer& source,
+    std::unique_ptr<caf::tracing_data>& dst) const override
+  {
+    return deserialize_impl(source, dst);
+  }
+
+  template <class Deserializer>
+  typename Deserializer::result_type
+  deserialize_impl(Deserializer& source,
+    std::unique_ptr<caf::tracing_data>& dst) const
+  {
+    std::string value;
+    if (auto err = source(value))
+      return err;
+    dst = std::make_unique<trc_data>(std::move(value));
+    return {};
+  }
+};
+
+struct prof : caf::actor_profiler
+{
+  void add_actor(const caf::local_actor&,                                             
+                   const caf::local_actor*) override
+                   {}
+    
+    void remove_actor(const caf::local_actor&) override{}
+    
+    void before_processing(const caf::local_actor& actor, 
+                           const caf::mailbox_element& element) override
+    {
+      if (element.tracing_id == nullptr) {
+        fprintf(stderr, "Unexpected error: tracing_id was null in before_processing!\n");
+        return;
+      } 
+      const auto* p = dynamic_cast<const trc_data*>(element.tracing_id.get());
+      if (p == nullptr) {
+        fprintf(stderr, "Couldn't downcast in before_processing.\n");
+        return;
+      }
+      printf("before_processing got \"%s\" tracing_id\n", p->s.c_str());
+    }               
+    
+    void after_processing(const caf::local_actor&,
+                          caf::invoke_message_result) override{}
+  
+    void before_sending(const caf::local_actor&,
+                        caf::mailbox_element& element) override
+    {
+      element.tracing_id = std::make_unique<trc_data>("before_sending put this here");
+    }
+  
+    void before_sending_scheduled(const caf::local_actor& self,   
+                                  caf::actor_clock::time_point timeout,                 
+                                  caf::mailbox_element& element) override
+    {
+      element.tracing_id = std::make_unique<trc_data>("before_sending_scheduled put this here");
+    }
+};
+
+caf::behavior actor1(caf::event_based_actor* self)
+{
+  return {
+    [self](std::string s) {
+      caf::aout(self) << "actor1 received: " << s << std::endl;
+      return "Thanks for sending: \"" + s + "\"!";
+    },
+  };
+}
+
+void actor2(caf::event_based_actor* self, const caf::actor& buddy)
+{
+  self->request(buddy, caf::infinite, std::string("Hi, I'm actor2."))
+      .then([self](const std::string& res) {
+        caf::aout(self) << "actor2 received: " << res << std::endl;
+      });
+}
+
+struct config : caf::actor_system_config
+{
+  prof profiler_;
+  trc_data_fac data_factory_;
+
+  config() {
+    profiler = &profiler_;
+    tracing_context = &data_factory_;
+  }
+};
+
+constexpr char anyaddr[] = "0.0.0.0";
+constexpr uint16_t port = 1337;
+
+#define USE_IO
+void caf_main1(caf::actory_system& sys, const config& config)
+{
+#ifdef USE_IO
+  const auto actor = sys.spawn(&actor1);
+  const auto exp_port = caf::io::publish(actor, port, anyaddr);
+  if (!exp_port)
+    fprintf(stderr, "Couldn't publish actor1 on %s:%" PRIu16 "!\n", anyaddr, port);
+#endif
+}
+
+void caf_main2(caf::actor_system& sys, const config& config)
+{
+#ifdef USE_IO
+  const auto exp_actor = caf::io::remote_actor(sys, anyaddr, port);
+
+  if (!exp_actor) {
+    fprintf(stderr, "Could not connect to remote actor!\n");
+    return;
+  }
+  
+  sys.spawn(&actor2, *exp_actor);
+#endif
+}
+
+int main(int argc, char** argv)
+{                                            
+  caf::exec_main_init_meta_objects<caf::io::middleman>();                           
+  caf::core::init_global_meta_objects();                                     
+  
+  auto fut1 = std::async(std::launch::async, []{ return caf::exec_main<caf::io::middleman>(caf_main1, argc, argv);});            
+
+  auto fut2 = std::async(std::launch::async, []{
+    return caf::exec_main<caf::io_middleman>(caf_main2, argc, argv);
+  });
+
+  return fut1.get() | fut2.get();
+}
