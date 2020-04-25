b82c29e887a36101560f50aa82850a5f1ab0f845
CppPhil <CppPhil@users.noreply.github.com>
2020-04-26 00:43:48 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ struct trc_data : caf::tracing_data {
   std::string s;
 
   trc_data(std::string str) : s(std::move(str)) {
+    // nop
   }
 
   caf::error serialize(caf::serializer& sink) const override {
@@ -54,9 +55,11 @@ thread_local std::string data;
 
 struct prof : caf::actor_profiler {
   void add_actor(const caf::local_actor&, const caf::local_actor*) override {
+    // nop
   }
 
   void remove_actor(const caf::local_actor&) override {
+    // nop
   }
 
   void before_processing(const caf::local_actor& actor,
@@ -76,6 +79,7 @@ struct prof : caf::actor_profiler {
 
   void after_processing(const caf::local_actor&,
                         caf::invoke_message_result) override {
+    // nop
   }
 
   void before_sending(const caf::local_actor&,
@@ -93,8 +97,8 @@ struct prof : caf::actor_profiler {
 caf::behavior actor1(caf::event_based_actor* self) {
   return {
     [=](std::string s) {
-      caf::aout(self) << "actor1 received: " << s << " tracing_id: \"" << data << "\""
-                      << std::endl;
+      caf::aout(self) << "actor1 received: \"" << s << "\" tracing_id: \""
+                      << data << "\"" << std::endl;
       data = "actor1 put this here";
       return "Thanks for sending: \"" + s + "\"!";
     },
@@ -105,8 +109,8 @@ void actor2(caf::event_based_actor* self, const caf::actor& buddy) {
   data = "actor2 put this here";
   self->request(buddy, caf::infinite, std::string("Hi, I'm actor2."))
     .then([=](const std::string& res) {
-      caf::aout(self) << "actor2 received: " << res << " tracing_id: \"" << data
-                      << "\"" << std::endl;
+      caf::aout(self) << "actor2 received: \"" << res << "\" tracing_id: \""
+                      << data << "\"" << std::endl;
     });
 }
 
@@ -123,26 +127,27 @@ struct config : caf::actor_system_config {
 constexpr char anyaddr[] = "0.0.0.0";
 constexpr uint16_t port = 1337;
 
-#define USE_IO
-void caf_main1(caf::actor_system& sys, const config& config) {
+//#define USE_IO
+void caf_main1(caf::actor_system& sys, const config&) {
 #ifdef USE_IO
   const auto actor = sys.spawn(&actor1);
   const auto exp_port = caf::io::publish(actor, port, anyaddr);
   if (!exp_port)
     fprintf(stderr, "Couldn't publish actor1 on %s:%" PRIu16 "!\n", anyaddr,
             port);
+#else
+  const auto a1 = sys.spawn(&actor1);
+  sys.spawn(&actor2, a1);
 #endif
 }
 
-void caf_main2(caf::actor_system& sys, const config& config) {
+void caf_main2([[maybe_unused]] caf::actor_system& sys, const config&) {
 #ifdef USE_IO
   const auto exp_actor = caf::io::remote_actor(sys, anyaddr, port);
-
   if (!exp_actor) {
     fprintf(stderr, "Could not connect to remote actor!\n");
     return;
   }
-
   sys.spawn(&actor2, *exp_actor);
 #endif
 }
@@ -153,9 +158,13 @@ int main(int argc, char** argv) {
   auto fut1 = std::async(std::launch::async, [=] {
     return caf::exec_main<caf::io::middleman>(caf_main1, argc, argv);
   });
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+#ifdef USE_IO
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
   auto fut2 = std::async(std::launch::async, [=] {
     return caf::exec_main<caf::io::middleman>(caf_main2, argc, argv);
   });
   return fut1.get() | fut2.get();
+#else
+  return fut1.get();
+#endif
 }
